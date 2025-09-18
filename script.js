b6b51a477ab21be4488c10dfd4a3ae72876c7920
RTArony <arony.radia@gmail.com>
2025-09-18 13:48:19 +0600

    cart & active satus

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const catagoryNameDisplay = (categoryArray)=>{
     const parent= document.getElementById("category-name")
     for(let category of categoryArray){
         const newCategory= document.createElement('div');
-        newCategory.innerHTML=`<p onClick="categoryPlantShow(${category.id})" class="text-xl my-2 text-center">${category.category_name}</p>`
+        newCategory.innerHTML=`<button id="category-btn-${category.id}" onClick="categoryPlantShow(${category.id})" class="text-xl my-2 ml-5 hover:bg-green-200 p-2 rounded-lg all-btn">${category.category_name}</button>`
         parent.appendChild(newCategory)
     }
     
@@ -26,32 +26,48 @@ const PlantsShow = (plants)=> {
         plantCard.innerHTML=`
                     <div class=" bg-white rounded-xl p-4 space-y-2">
                         <img class="h-100" src="${plant.image}" alt="">
-                        <button onclick="loadPlantDetails(${plant.id})" class="font-bold text-xl">${plant.name}</button>
+                        <button onclick="loadPlantDetails(${plant.id})" class="font-bold text-xl hover:bg-green-200 p-2 rounded-lg">${plant.name}</button>
                         <p class="text-gray-500">${plant.description}</p>
                         <div class="flex justify-between items-center">
                             <p class="bg-[#DCFCE7] text-[#15803D] font-bold rounded-full p-2">${plant.category}</p>
                             <p class="font-bold">$<span>${plant.price}</span></p>
                         </div>
-                        <button onclick="addTocart(${plant.price},'${plant.name}', ${plant.id})" class="bg-[#15803D] text-white font-semibold rounded-full p-2 w-full">Add to Cart</button>
+                        <button onclick="addTocart(${plant.price},'${plant.name}', ${plant.id})" class="bg-[#15803D] text-white font-semibold rounded-full p-2 w-full hover:bg-green-600 ">Add to Cart</button>
                     </div>`
         parent.appendChild(plantCard)
     }
 }
 
 
-// category based plant show 
+// category based plant show & active status
 const categoryPlantShow=(id)=>{
     const url= `https://openapi.programming-hero.com/api/category/${id}`
     fetch(url)
     .then(res=>res.json())
-    .then(data=>PlantsShow(data.plants))
+    .then(data=>{
+        const allBtn=document.querySelectorAll(".all-btn")
+        allBtn.forEach((btn)=>btn.classList.remove("bg-green-700", "text-white"))
+
+        const btn=document.getElementById(`category-btn-${id}`)
+        btn.classList.add("bg-green-700", "text-white")
+
+        PlantsShow(data.plants)
+    })
 }
 
 // all tree button work
 document.getElementById("all-tree-btn").addEventListener("click", ()=>{
     fetch("https://openapi.programming-hero.com/api/plants")
     .then(res=> res.json())
-    .then(data=> PlantsShow(data.plants))
+    .then(data=> {
+        const allBtn=document.querySelectorAll(".all-btn")
+        allBtn.forEach((btn)=>btn.classList.remove("bg-green-700", "text-white"))
+
+        const allTreeBtn=document.getElementById("all-tree-btn")
+        allTreeBtn.classList.add("bg-green-700", "text-white")
+        
+        PlantsShow(data.plants)
+    })
 })
 
 
@@ -66,7 +82,7 @@ const showPlantDetails=(plant)=>{
     const parent=document.getElementById("tree-details")
     parent.innerHTML=`<p class="font-bold text-xl">Product id: <span>${plant.id}</span></p>
                     <img class="h-100" src="${plant.image}" alt="">
-                    <button class="font-bold text-xl">${plant.name}</button>
+                    <p class="font-bold text-xl">${plant.name}</p>
                     <p class="text-gray-500">${plant.description}</p>
                     <div class="flex justify-between items-center">
                         <p class="bg-[#DCFCE7] text-[#15803D] font-bold rounded-full p-2">${plant.category}</p>
@@ -81,29 +97,43 @@ const showPlantDetails=(plant)=>{
     document.getElementById("my_modal_1").showModal()
 }
 
-let totalPrice=0
-const addTocart=(price,name,id)=>{
-    const parent =document.getElementById("cart-container")
-    const newitem=document.createElement('div')
-    newitem.innerHTML=`<div class="flex justify-between items-center bg-[#F0FDF4] m-3 p-2 rounded-lg">
-                            <div>
-                                <p class="font-bold text-xl">${name}</p>
-                                <p class="text-gray-500 text-lg">$ ${price} </p>
-                            </div>
-                            <div>
-                                <button id="cross-btn${id}" class="text-gray-500 text-xl">✕</button>
-                            </div>
-                        </div>`
-    parent.appendChild(newitem)
-    totalPrice += price
-    const totalPriceShow = document.getElementById("total-price")
-    totalPriceShow.innerText = totalPrice
 
-    document.getElementById(`cross-btn${id}`).addEventListener("click",()=>{
-        parent.removeChild(newitem)
-        totalPrice -= price
-        const totalPriceShow = document.getElementById("total-price")
-        totalPriceShow.innerText = totalPrice
-    })
-}
+
+let totalPrice = 0
+const addTocart = (price, name) => {
+  const parent = document.getElementById("cart-container")
+
+  const newitem = document.createElement("div");
+  newitem.className =
+    "flex justify-between items-center bg-[#F0FDF4] m-3 p-2 rounded-lg"
+
+  const leftDiv = document.createElement("div")
+  leftDiv.innerHTML = `
+    <p class="font-bold text-xl">${name}</p>
+    <p class="text-gray-500 text-lg">$ ${price}</p>
+  `
+
+  const rightDiv = document.createElement("div")
+  const crossBtn = document.createElement("button")
+  crossBtn.className = "text-gray-500 text-xl hover:bg-gray-300 hover:text-black p-3 rounded-md"
+  crossBtn.textContent = "✕"
+
+  
+  crossBtn.addEventListener("click", () => {
+    parent.removeChild(newitem);   
+    totalPrice -= price;           
+    document.getElementById("total-price").innerText = totalPrice
+  });
+
+  rightDiv.appendChild(crossBtn)
+  newitem.appendChild(leftDiv)
+  newitem.appendChild(rightDiv)
+
+ 
+  parent.appendChild(newitem)
+
+  
+  totalPrice += price
+  document.getElementById("total-price").innerText = totalPrice
+};
 
